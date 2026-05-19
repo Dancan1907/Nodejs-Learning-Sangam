d2d4bf1d705993506c413b10027d70e91f523953
Dancan1907 <dancankalerwa@gmail.com>
2026-05-19 17:04:02 +0300

    1.0 Intro Learned  how to run & contruct a file

---
+++ index.js
@@ -0,0 +1,7 @@
+// Root file of this folder, which is the entry point for the package. It exports all the necessary components and functions for users to interact with the package.
+
+console.log("Hello Node js")
+
+// Intro how to run a file in node js
+// To run a file in Node.js, you can use the command line. Open your terminal and navigate to the directory where your JavaScript file is located. Then, you can run the file using the following command: `node filename.js`   
+// For example, if your file is named `index.js`, you would run: `node index.js`. This will execute the JavaScript code in the file and display any output in the terminal.
---
+++ sum.js
@@ -0,0 +1,8 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum(5, 10)); // Output: 15
+
+//Example of a file that can run apart from index.js. This file contains a simple function to
+//  calculate the sum of two numbers and logs the result to the console. You can run this file 
+// using Node.js by executing `node sum.js` in the terminal, assuming the file is named `sum.js`. 
